472d41ef02b483c9ca14835fb0876bd5203b8633
Darshika Premaranjan <117564041+darship19@users.noreply.github.com>
2024-11-06 18:19:01 +0530

    Update Home.js

--- client/src/controller/Home.js
+++ client/src/controller/Home.js
@@ -11,7 +11,7 @@ function Home() {
             <div className="home-box">
                 <Logout />
                 <div style={{ padding: '20px' }}>
-                    <h1>Home</h1>
+                    <h1>Welcome to the Home page</h1>
                     <h2>Your Attendance is marked</h2>
                     {username && <h2>Welcome to the home page, {username}!</h2>}
                 </div>
